868ab8b00612f8a7d5399fe8b42fbbc76635898b
amin ahmed <aminahmed24@gmail.com>
2024-11-12 16:08:48 +0200

    2. Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,6 +3,7 @@
 Route module for the API
 """
 from os import getenv
+from typing import Literal
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -15,17 +16,24 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
-def not_found(error) -> str:
+def not_found(error) -> tuple[str, Literal[404]]:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
+def unauthorized(error) -> tuple[str, Literal[401]]:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> tuple[str, Literal[403]]:
+    """ Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,6 +26,15 @@ def stats() -> str:
     return jsonify(stats)
 
 
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden():
+    """ GET /api/v1/forbidden
+    Return:
+      - Forbidden page
+    """
+    abort(403)
+
+
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
     """ GET /api/v1/unauthorized
