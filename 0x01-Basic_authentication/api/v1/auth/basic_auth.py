ee480c7f8cd5a042bd2a40142bfdf72bbdafc87c
amin ahmed <aminahmed24@gmail.com>
2024-11-12 23:30:45 +0200

    8. Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 "Basic Authentication"
+from base64 import b64decode
 from api.v1.auth.auth import Auth
 
 
@@ -16,3 +17,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.removeprefix('Basic ')
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        "returns the decoded value of a Base64 string"
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
