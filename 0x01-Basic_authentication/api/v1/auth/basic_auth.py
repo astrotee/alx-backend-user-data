47e9cb7763b08752decae0f93c457e2b37886d3f
amin ahmed <aminahmed24@gmail.com>
2024-11-12 23:45:03 +0200

    9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 "Basic Authentication"
 from base64 import b64decode
+from typing import Tuple
 from api.v1.auth.auth import Auth
 
 
@@ -30,3 +31,15 @@ class BasicAuth(Auth):
             return b64decode(base64_authorization_header).decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> Tuple[str, str]:
+        "returns the user email and password from the Base64 decoded value"
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        if not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        if ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return tuple(decoded_base64_authorization_header.split(':'))
