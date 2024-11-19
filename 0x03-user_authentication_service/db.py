72c12204eb8d95139d210a2f298f5a757747ee49
amin ahmed <aminahmed24@gmail.com>
2024-11-19 19:41:23 +0200

    2. Find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,8 +2,10 @@
 """DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
@@ -37,4 +39,15 @@ class DB:
         self._session.add(user)
         self._session.commit()
 
+        return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """search for a user
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        except InvalidRequestError as e:
+            raise e
+        if user is None:
+            raise NoResultFound
         return user
