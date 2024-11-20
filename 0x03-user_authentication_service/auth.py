b36bccc360dcfc5c9054f8dea93275b4c6bdaee6
amin ahmed <aminahmed24@gmail.com>
2024-11-20 02:11:51 +0200

    8. Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,3 +27,13 @@ class Auth:
             return self._db.add_user(email, _hash_password(password).decode())
         else:
             raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """check if the creds are valid
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(),
+                                  user.hashed_password.encode())
+        except NoResultFound:
+            return False
