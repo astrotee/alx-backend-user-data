4dd5c7bb20ea1d94979b8f3cf23fa8e050145698
amin ahmed <aminahmed24@gmail.com>
2024-11-20 15:01:27 +0200

    12. Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,3 +56,14 @@ class Auth:
         sid = _generate_uuid()
         self._db.update_user(user.id, session_id=sid)
         return sid
+
+    def get_user_from_session_id(self, session_id: str) -> Optional[User]:
+        """get a user using session_id
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
